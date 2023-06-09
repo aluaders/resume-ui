d913f9ed35e9b748109ef1735af42d5c698fe61c
aluaders <TonyLuaders@gmail.com>
2023-06-09 16:37:51 -0400

    Added templates and logic to easy create component

--- src/stories/mui/Button/Button.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from 'react';
-import { Button as MuiButton, ButtonProps as MuiButtonProps } from '@mui/material';
- 
-export const Button: FC<MuiButtonProps> = ({ children, ...args }) => (
-  <MuiButton {...args}>{children}</MuiButton>
-);
---
+++ src/utils/generate-react-cli/templates/TemplateName.mock.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function TemplateNameMock() {
+    return (<div data-testid="TemplateName-mock"/>);
+}
